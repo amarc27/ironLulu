a7827b3955acf99fe81d8aeb6ac6a62a84255523
Medhi-PLM <medhi.plumasseau@gmail.com>
2018-02-03 12:35:08 +0100

    Modify Edit profile routes

--- routes/profile.js
+++ routes/profile.js
@@ -1,18 +1,30 @@
 const express = require('express');
 const router  = express.Router();
 const User = require('../models/user');
+const Campaign = require('../models/campaign');
 const passport = require("passport");
 const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
 
-
-
-
 router.get('/', ensureLoggedIn('/login'), (req, res, next) => {
-    res.render('profile/edit', {user: req.user} )
+    res.redirect('/profile/'+ req.user._id)
 });
-  
-  
-router.post('/', ensureLoggedIn('/login'), (req, res, next) => {
+
+//Show Profile
+router.get('/:id', ensureLoggedIn('/login'), (req, res, next) => {
+    res.render('profile/show', {user: req.user} )
+});
+
+//Edit profile
+router.get('/:id/edit', ensureLoggedIn('/login'), (req, res, next) => {
+  User.findById(req.params.id, (err, user) => {
+    if (err)       { return next(err) }
+    if (!user) { return next(new Error("404")) }
+    return res.render('profile/edit', {user: req.user} )
+  });
+});
+
+//Post Edit profile
+router.post('/:id', ensureLoggedIn('/login'), (req, res, next) => {
     const updates = {
       firstname: req.body.firstname,
       lastname: req.body.lastname,
@@ -20,18 +32,20 @@ router.post('/', ensureLoggedIn('/login'), (req, res, next) => {
       password: req.body.password,
       description: req.body.description
     };
-  
-    User.findByIdAndUpdate(req.user._id, updates, (err, profile) => {
+
+    User.findByIdAndUpdate(req.params.id, updates, (err, user) => {
+      console.log(err,user);
       if (err) {
+        console.log(err);
         return res.render('profile/edit', {
-          profile,
-          errors: profile.errors
+          user,
+          errors: err
         });
       }
-      if (!profile) {
+      if (!user) {
         return next(new Error('404'));
       }
-      return res.redirect(`/campaign/all`);
+      return res.redirect(`/profile/${user._id}`);
     });
 });
 
