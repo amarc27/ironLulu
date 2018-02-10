43fbfb0435a66670df965162c1c4e0181e6a1826
Medhi-PLM <medhi.plumasseau@gmail.com>
2018-02-10 07:33:32 +0100

    Ajout des infos des campagnes sur les marker de la maps

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,5 +1,7 @@
 var map;
 var myMarker;
+var infoWindow;
+var contentString;
 function initMap() {
   console.log(myCampaigns);
 
@@ -7,14 +9,33 @@ function initMap() {
     center: {lat: 48.8566, lng: 2.3522},
     zoom: 12
   });
+
+
   myCampaigns.forEach((campaign) => {
+    contentString = '<div>'+
+    '<h1>'+campaign.name+'</h1>'+
+    '<p>'+campaign.description+'</p>'+
+    '</div>',
+
+    console.log("DEBUG CONTENT STRING", contentString),
+
+    infoWindow = new google.maps.InfoWindow({
+      content: contentString
+    }),
+
     myMarker = new google.maps.Marker({
       position: {
         lat: campaign.location.coordinates[0],
         lng: campaign.location.coordinates[1]
       },
       map: map,
-      title: "I'm here"
-    })
+      title: campaign.name,
+      info: contentString
+    }),
+
+    google.maps.event.addListener(myMarker, 'click', function() {
+      infoWindow.setContent(this.info);
+      infoWindow.open(map, this);
+    });
   });
 }
